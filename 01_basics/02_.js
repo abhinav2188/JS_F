0041d69eb11d2623a2a17278aba5aed998c6a529
abhinav rastogi <56015300+abhinav2188@users.noreply.github.com>
2024-11-21 07:18:12 +0000

    datatypes

---
+++ 01_basics/02_.js
@@ -0,0 +1,26 @@
+"use strict"
+// this is to tell that we want to use new version of js
+
+// alert(3+5) // doesn't work in node
+
+// tc39.es, mdn
+
+// datatypes
+
+let str = "abhi"
+let num = 3
+let bool = false
+
+/*
+number = 2^53
+bigint
+string 
+boolean = true/false
+null => signifies empty value
+undefined => variable declared but not defined
+symbol => used in components react (for uniqueness)
+object
+*/
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefind
